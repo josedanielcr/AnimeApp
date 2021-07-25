e0e315bbb08e06347b3a53ed46bd7cbf1c00a88d
Jose Daniel <71776938+josedanielcr@users.noreply.github.com>
2021-07-24 21:32:40 -0600

    Estructura inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// CUSTOM MODULES
+import { ComponentsModule } from './components/components.module';
+import { PagesModule } from './pages/pages.module';
+import { PipesModule } from './pipes/pipes.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentsModule,
+    PagesModule,
+    PipesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
